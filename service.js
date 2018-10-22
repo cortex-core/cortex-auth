968d4dd069c41ba4c3a2700a79bc8695a2c5568f
Basar Turgut <bturgut@twilio.com>
2018-10-23 00:16:18 +0300

    Add unfinished authenticate endpoint

--- service.js
+++ service.js
@@ -1,12 +1,68 @@
 const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const winston = require('winston');
+const jwt = require('jsonwebtoken');
+
+const TTL = 180;
+
+const logger = winston.createLogger({
+    level: 'silly',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
 
 let app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.post('/authenticate', function(req, res) {
+    logger.debug("Authenticate method is being called");
+    let secret = req.body.secret;
+
+    // get the decoded payload and header
+    var decoded = jwt.decode(secret, {complete: true});
+    console.log(decoded.header);
+    console.log(decoded.payload);
+
+    let account_id = decoded.payload.account_id;
+    let created = decoded.payload.created || new Date().getTime();
+
+    if(created > new Date().getTime() + TTL * 1000000) {
+        res.send(401, 'Invalid token');
+    }
+
+    // verify token
+    // TODO : Implement user's public key retrival from db
+    var cert = '-----BEGIN PUBLIC KEY-----\n' +
+        'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd\n' +
+        'UWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs\n' +
+        'HUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D\n' +
+        'o2kQ+X5xK9cipRgEKwIDAQAB\n' +
+        '-----END PUBLIC KEY-----';  // get public key
+    jwt.verify(secret, cert, function(err, decoded) {
+        if(err != null) {
+            res.status(401).send(err);
+            return;
+        }
+        // TODO : Our assigned secret_key for user
+        // TODO : Put data into JWT payload
+        // TODO : Sign
+        res.json({
+            token: 'test',
+            role: 'undefined',
+            expiry: new Date().toUTCString(),
+            created: new Date().toUTCString(),
+            updated: new Date().toUTCString()
+        });
+    });
+});
 
 app.listen(8080, function() {
   console.log("0auth service listening 8080")
--- test/service-test.js
+++ test/service-test.js
@@ -2,41 +2,58 @@ const chai = require('chai');
 const chai_http = require('chai-http');
 const chai_date_string = require('chai-date-string');
 const service = require('./../service');
+
 chai.should();
 chai.use(chai_http);
 chai.use(chai_date_string);
 
 describe('0auth Baseline', function() {
-  it('should provide JWT via /authenticate POST endpoint', function(done){
-      let auth_request = { secret: 'user_secret'};
-      chai.request(service)
-          .post('/authenticate')
-          .send(auth_request)
-          .end(function(err, res){
-              if(err){
-                  err.should.equal(null);
-              }
-              res.should.have.status(200);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property('token');
-              res.body.token.should.be.a.dateString();
-              res.body.should.have.property('role');
-              res.body.should.have.property('expiry');
-              res.body.should.have.property('created');
-              res.body.should.have.property('updated');
-              done();
-          });
-  });
-  it('should provide JWKS via /jwks GET endpoint', function(done){
-      chai.request(service)
-      .get('/jwks')
-      .end(function(err, res){
-          if(err){
-              err.should.equal(null);
-          }
-          res.statusCode.should.equal(200);
-          done();
-      });
-  });
+    it('should provide JWT via /authenticate POST endpoint', function(done){
+        let auth_request = { account_id: '1234567890', secret: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIzNDU2Nzg5MCIsImNyZWF0ZWQiOiIxNTQwMjQxODYyIn0.bV-2hSoqaYnWvvb0Wje8FywMFKFX9ltI8Mc_AbBHt7Xg4k6LgiNY9JRIf_4fahSQZa_mOLB5MGz6Q4mI-E8OlTcMS2Ol7pfcRaB0Z5OqYuoAJymDuF1lLC2GEz-Dx-0ob6qO1b0fSzof53Qdv5KnTtvfrorXqew1inoR6tz21EE' };
+        chai.request(service)
+            .post('/authenticate')
+            .send(auth_request)
+            .end(function(err, res){
+                if(err){
+                    err.should.equal(null);
+                }
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('token');
+                res.body.should.have.property('role');
+                res.body.should.have.property('expiry');
+                res.body.should.have.property('created');
+                res.body.should.have.property('updated');
+                done();
+            });
+    });
+    it('should provide JWKS via /jwks GET endpoint', function(done){
+        chai.request(service)
+            .get('/jwks')
+            .end(function(err, res){
+                if(err){
+                    err.should.equal(null);
+                }
+                res.statusCode.should.equal(200);
+                done();
+            });
+    });
+    it('should provide validation response via /validate POST endpoint', function(done){
+        let validation_request = { access_token: 'auth service generated token in JWT format' };
+        chai.request(service)
+            .post('/validate')
+            .send(validation_request)
+            .end(function(err, res){
+                if(err){
+                    err.should.equal(null);
+                }
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('isValid');
+                res.body.should.have.property('role');
+                done();
+            });
+    });
 });
